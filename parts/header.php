ce7e49f6acc623495f650157611f57bcd7988d44
Gena <gena@e64.net>
2017-03-24 14:36:47 +0100

    mise en place de la structure

--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,15 @@
-put some header here
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="semantic-ui-css/semantic.min.css">
+
+</head>
+<body>
+	<header>
+		<?php include "menu.php" ?>
+	</header>
+	
+</body>
---
+++ parts/menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Menu</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="semantic-ui-css/semantic.min.css">
+</head>
+<div class="ui three item menu">
+	<a class="active item">Accueil</a>
+	<a class="item">Bio</a>
+	<a class="item">Contact</a>
+</div>
+<body>
+	
+</body>
+</html>
+
+
+

187395922215d1b0feb207abb7a757333a66415f
Gena <gena@e64.net>
2017-03-26 21:31:10 +0200

    Ajout de Semantic UI

--- parts/menu.php
+++ parts/menu.php
@@ -1,4 +1,4 @@
-<div class="ui three item menu">
+<div class="ui four item menu">
 	<a class="active item" href="index.php">Accueil</a>
 	<a class="item" href="index.php?bio">Bio</a>
 	<a class="item" href="index.php?contact">Contact</a>
